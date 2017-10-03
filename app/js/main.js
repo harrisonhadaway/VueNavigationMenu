5b6247dd390acd3eef3be7fe11c36b8509e7f97d
harrisonhadaway <harrisonleehadaway@gmail.com>
2017-10-03 14:34:06 -0400

    Simplified and tricky CSS using VUE

--- app/js/main.js
+++ app/js/main.js
@@ -1,45 +1,12 @@
 var menu = new Vue( {
 	el : '#menu',
 	data : {
-		homeActive : false,
-		projectsActive : false,
-		servicesActive : false,
-		contactActive : false,
-		message : 'Please click on a menu item'
+			active : false
 	},
 	methods : {
-		makeActive : function( item, label ) {
-			console.log( 'makeActive');
-			console.log( 'makeActive - item[', item, '] label[', label, ']' ); 
-		
-			//Remove active class from the previous anchor
-			this.homeActive = false;
-			this.projectsActive = false;
-			this.servicesActive = false;
-			this.contactActive = false;
-			//Add active class to the selector anchor (item)
-			switch( item ) {
-				case 'homeMenu':
-					this.homeActive = true;
-					break;
-				case 'projectsMenu':
-					this.projectsActive = true;
-					break;
-				case 'servicesMenu':
-					this.servicesActive = true;
-					break;
-				case 'contactMenu':
-					this.contactActive = true;
-					break;
+			makeActive : function( item ) {
+					this.active = item;
 
 			}
-
-
-			// Change the text of the message
-			this.message = 'You chose ' + label;
-
-		}
-
-
-	}
+			}
 });
